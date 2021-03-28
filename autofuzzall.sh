4027cfeab92521ae32f8ddd765e89b70cc02b4e5
Shizhi He <heshizhi@126.com>
2021-03-28 16:28:21 +0800

    Update autofuzzall.sh

--- autofuzzall.sh
+++ autofuzzall.sh
@@ -4,7 +4,7 @@ rm -rf $dir$dir2
 for file in `ls $dir | grep .fu`
 do
   echo $dir$file
-  python3 firmking.py -s 2 -r 1-10 --log $dir$file 192.168.0.1
+  python3 firmking.py -s 2 -r 1-15 --log $dir$file 192.168.0.1
     
 done
 find . -name "*Refused.txt*"
